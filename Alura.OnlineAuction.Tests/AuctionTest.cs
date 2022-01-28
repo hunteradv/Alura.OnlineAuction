365b6ebca78e143474cddd64d558db724e41e1fd
hunteradv <gustavopaulody@gmail.com>
2022-01-27 23:44:10 -0300

    Melhorando testes, tornando os 3 testes de vencedor do leilão em 1.

--- Alura.OnlineAuction.Tests/AuctionTest.cs
+++ Alura.OnlineAuction.Tests/AuctionTest.cs
@@ -5,46 +5,108 @@ namespace Alura.OnlineAuction.Tests
 {
     public class AuctionTest
     {
-        [Fact]
-        public static void AuctionWithMultipleBids()
-        {
 
+        //Verifica se a mesma pessoa realiza dois lances, ainda encontra o vencedor correto.
+        [Fact]
+        public static void AuctionWithAtLeastOneBid()
+        {
+            //arranje - cenário
+            //Dado leilão de pelo menos um lance
             var auctionTest = new Auction("Pintura Van Gogh");
 
             var client1 = new Client("Cliente 1", auctionTest);
             var client2 = new Client("Cliente 2", auctionTest);
             var client3 = new Client("Cliente 3", auctionTest);
 
-            client1.Auction.ReceiveBid(client1, 1000);
-            client2.Auction.ReceiveBid(client2, 1100);
-            client3.Auction.ReceiveBid(client3, 500);
+            auctionTest.ReceiveBid(client1, 1000);
+            auctionTest.ReceiveBid(client2, 1100);
+            auctionTest.ReceiveBid(client3, 1200);
+            auctionTest.ReceiveBid(client2, 2000);
 
-            //Act
+            //Act - método sob teste
+            //Quando o leilão termina e decide um vencedor
             auctionTest.FinishTrading();
 
-            var expectedValue = 1100.0;
-            var valueObtained = auctionTest.Winner.Value;
+            var expectedValue = 2000;
 
             //Assert
-            Assert.Equal(expectedValue, valueObtained);
-
+            //Então o valor esperado é o maior dado
+            //E o Cliente vencedor é o que deu o maior lance
+            Assert.Equal(client2, auctionTest.Winner.Client);
+            Assert.Equal(expectedValue, auctionTest.Winner.Value);
         }
 
-        [Fact]
-        public static void AuctionWithOneBid()
-        {
-            var auctionTest = new Auction("Pintura Van Gogh");
+        
 
-            var client1 = new Client("Cliente 1", auctionTest);
+        ////Verifica se a mesma pessoa realiza dois lances, ainda encontra o vencedor correto.
+        //[Fact]
+        //public static void AuctionWithMultibleBidsOfOneClient()
+        //{
+        //    //arranje - cenário
+        //    var auctionTest = new Auction("Pintura Van Gogh");
 
-            client1.Auction.ReceiveBid(client1, 1000);
+        //    var client1 = new Client("Cliente 1", auctionTest);
+        //    var client2 = new Client("Cliente 2", auctionTest);
+        //    var client3 = new Client("Cliente 3", auctionTest);
 
-            auctionTest.FinishTrading();
+        //    auctionTest.ReceiveBid(client1, 1000);
+        //    auctionTest.ReceiveBid(client2, 1100);
+        //    auctionTest.ReceiveBid(client3, 1200);
+        //    auctionTest.ReceiveBid(client2, 2000);
 
-            var expectedValue = 1000;
-            var valueObtained = auctionTest.Winner.Value;
+        //    //Act - método sob teste
+        //    auctionTest.FinishTrading();
 
-            Assert.Equal(expectedValue, valueObtained);
-        }
+        //    var expectedValue = 2000;
+
+        //    //Assert
+        //    Assert.Equal(client2, auctionTest.Winner.Client);
+        //    Assert.Equal(expectedValue, auctionTest.Winner.Value);
+        //}
+
+        ////Verifica se houver vários lances, ainda encontra o vencedor correto.
+        //[Fact]
+        //public static void AuctionWithMultipleBids()
+        //{
+
+        //    var auctionTest = new Auction("Pintura Van Gogh");
+
+        //    var client1 = new Client("Cliente 1", auctionTest);
+        //    var client2 = new Client("Cliente 2", auctionTest);
+        //    var client3 = new Client("Cliente 3", auctionTest);
+
+        //    auctionTest.ReceiveBid(client1, 1000);
+        //    auctionTest.ReceiveBid(client2, 1100);
+        //    auctionTest.ReceiveBid(client3, 500);
+
+        //    //Act
+        //    auctionTest.FinishTrading();
+
+        //    var expectedValue = 1100.0;
+        //    var valueObtained = auctionTest.Winner.Value;
+
+        //    //Assert
+        //    Assert.Equal(expectedValue, valueObtained);
+
+        //}
+
+
+        ////Verifica se a mesma pessoa realiza dois lances, ainda encontra o vencedor correto.
+        //[Fact]
+        //public static void AuctionWithOneBid()
+        //{
+        //    var auctionTest = new Auction("Pintura Van Gogh");
+
+        //    var client1 = new Client("Cliente 1", auctionTest);
+
+        //    auctionTest.ReceiveBid(client1, 1000);
+
+        //    auctionTest.FinishTrading();
+
+        //    var expectedValue = 1000;
+        //    var valueObtained = auctionTest.Winner.Value;
+
+        //    Assert.Equal(expectedValue, valueObtained);
+        //}
     }
 }
